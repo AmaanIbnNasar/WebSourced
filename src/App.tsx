211a1174df795ebea39fdbdf3ac17ba3a4121af2
Amaan-WSL <amaan5260@gmail.com>
2022-03-05 14:24:19 +0000

    Initial commit - basic file structure and setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+    return <figure className="md:flex bg-slate-100 rounded-xl">
+        <div className="pt-6 md:p-8 text-center">
+            <p className="text-lg font-medium">
+                "This is some text to test out some tailwind stuff"
+            </p>
+        </div>
+        </figure>
+          }
